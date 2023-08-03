4bf0cc09fa36745a2036a8f091910ac3082f523a
Polakrit Chulanutrakul <141233391+Polakrit-Chulanutrakul@users.noreply.github.com>
2023-08-03 17:44:47 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='mt-14 mx-auto max-w-6xl grid grid-cols-[40%_60%] '>
+      <div>
+        <div className='sticky top-14 grid grid-rows-[35%_45%_20%]'>
+          <div>
+            <div>Polakrit Chulanutrakul</div>
+            <div>Front-End Developer - open to work</div>
+            <div>Empowering Brands, Enchanting Customers</div>
+            <div>View Resume</div>
+          </div>
+          <div>Nav</div>
+          <div className='flex items-end'>Link</div>
+        </div>
+      </div>
+
+      <div>
+        <div>About</div>
+        <div className='mb-96'>Experiences</div>
+        <div className='mb-96'>Experiences</div>
+        <div className='mb-96'>Experiences</div>
+
+      </div>
+    </div>
+  )
+}
+
+export default App
